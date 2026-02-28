c35349f5c422f49466573d4de226193619b83e6d
3079103696chentao <3079103696@qq.com>
2026-02-28 12:11:45 +0800

    1.设计的鼠标远程控制的结构 2.实现了本地鼠标信息的获取、封装和发送

--- RemoteCtrl/RemoteClient/WatchDialog.cpp
+++ RemoteCtrl/RemoteClient/WatchDialog.cpp
@@ -6,6 +6,7 @@
 #include "afxdialogex.h"
 #include "WatchDialog.h"
 #include"RemoteClientDlg.h"
+#include"ClientSocket.h"
 
 // CWatchDialog 对话框
 
@@ -30,10 +31,27 @@ void CWatchDialog::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CWatchDialog, CDialogEx)
 	ON_WM_TIMER()
+	ON_WM_LBUTTONDBLCLK()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDBLCLK()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
 // CWatchDialog 消息处理程序
+
+CPoint CWatchDialog::UserPoint2RemoteScreenPoint(CPoint& point)
+{
+	//从屏幕坐标转化为客户端坐标
+	CRect clientRect;
+	ScreenToClient(&point);//获取在截取的屏幕上点击的位置坐标,本地坐标,把屏幕坐标换算成某个窗口客户区坐标
+	//本地坐标到远程坐标
+	m_picture.GetWindowRect(clientRect);
+	return CPoint(point.x * 1920 / clientRect.Width(), point.y * 1080 / clientRect.Height());
+}
 
 BOOL CWatchDialog::OnInitDialog()
 {
@@ -66,4 +84,62 @@ void CWatchDialog::OnTimer(UINT_PTR nIDEvent)
 		}
 	}
 	CDialogEx::OnTimer(nIDEvent);
+}
+
+void CWatchDialog::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	//坐标转换
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+	//封装
+	MOUSEEV event;
+	event.ptXY = remote;
+	event.nButton = 1; //左键
+	event.nAction = 1; //双击
+	//发送
+	
+
+	CDialogEx::OnLButtonDblClk(nFlags, point);
+}
+
+void CWatchDialog::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+void CWatchDialog::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialogEx::OnRButtonDblClk(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialogEx::OnRButtonDown(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialogEx::OnRButtonUp(nFlags, point);
+}
+
+void CWatchDialog::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialogEx::OnMouseMove(nFlags, point);
 }
